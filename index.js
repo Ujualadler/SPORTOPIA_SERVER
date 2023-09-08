6a487ccede558fb866ebb9188e5e305e0ee9b0b6
ujual <ujualk2000@gmail.com>
2023-09-08 19:36:59 +0530

    cors

--- index.js
+++ index.js
@@ -4,29 +4,26 @@ const dotenv = require("dotenv").config();
 const { connectDb } = require("./config/dbConnection");
 const socketIo = require("socket.io");
 
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+const app = express();
 
+app.use(express.json({ limit: "100mb", extended: true }));
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 app.use(cors(corsOptions));
 app.use(express.static("public"));
+
 app.use("/", userRouter);
 app.use("/turf", turfRouter);
 app.use("/admin", adminRouter);
 
-
 const userRouter = require("./routes/users.js");
 const turfRouter = require("./routes/turf.js");
 const adminRouter = require("./routes/admin.js");
-
-const app = express();
-app.use(express.json({ limit: "100mb", extended: true }));
-
-
-
 
 const PORT = process.env.MONGODB_PORT;
 
@@ -41,8 +38,7 @@ const io = socketIo(server, {
     origin: "*",
     credentials: true,
   },
-})
-
+});
 
 io.of("/chat").on("connection", (socket) => {
   socket.on("joinRoom", (clubId) => {
@@ -56,58 +52,62 @@ io.of("/chat").on("connection", (socket) => {
     // Emit the message to the specific room based on the clubId
     io.of("/chat").to(receivedClubId).emit("message", message, receivedClubId);
   });
-  socket.on("error" , err =>{
-    console.log('backend error ' , err);
-  })
+  socket.on("error", (err) => {
+    console.log("backend error ", err);
+  });
 
   socket.on("disconnect", (ev) => {
-    console.log("Socket disconnected" , ev);
+    console.log("Socket disconnected", ev);
   });
 });
 
-
-
 //  Turf booking socket
-let onBooking = {}
+let onBooking = {};
 
-io.of('/booking').on("connection", (socket) => {
+io.of("/booking").on("connection", (socket) => {
   socket.on("joinBooking", (turfId) => {
     // Join the specific room based on the turfId
     socket.join(turfId);
-    io.of('/booking').to(turfId).emit("message", turfId, onBooking[turfId]);
+    io.of("/booking").to(turfId).emit("message", turfId, onBooking[turfId]);
     console.log(`Socket joined room: ${turfId}`);
   });
 
   socket.on("updateBooking", (receivedturfId, date, slot, user) => {
     if (!Object.keys(onBooking).includes(receivedturfId))
-      onBooking[receivedturfId] = {}
-    
-    if (!onBooking[receivedturfId][date]) 
-      onBooking[receivedturfId][date] = [{slot: slot, user: user}]
-    else
-      onBooking[receivedturfId][date].push({slot, user})
-    
+      onBooking[receivedturfId] = {};
+
+    if (!onBooking[receivedturfId][date])
+      onBooking[receivedturfId][date] = [{ slot: slot, user: user }];
+    else onBooking[receivedturfId][date].push({ slot, user });
+
     console.log(`booking slot, ${slot}, on ${date} in: ${receivedturfId}`);
-    console.log(onBooking)
-    
+    console.log(onBooking);
+
     // Emit the message to the specific room based on the turfId
-    io.of('/booking').to(receivedturfId).emit("message", receivedturfId, onBooking[receivedturfId]);
+    io.of("/booking")
+      .to(receivedturfId)
+      .emit("message", receivedturfId, onBooking[receivedturfId]);
   });
 
   socket.on("removeBooking", (receivedturfId, date, slot, user) => {
-    onBooking[receivedturfId][date].splice(onBooking[receivedturfId][date].indexOf({slot, user}), 1);
-     
-    console.log(onBooking)
-    
+    onBooking[receivedturfId][date].splice(
+      onBooking[receivedturfId][date].indexOf({ slot, user }),
+      1
+    );
+
+    console.log(onBooking);
+
     // Emit the message to the specific room based on the turfId
-    io.of('/booking').to(receivedturfId).emit("message", receivedturfId, onBooking[receivedturfId]);
+    io.of("/booking")
+      .to(receivedturfId)
+      .emit("message", receivedturfId, onBooking[receivedturfId]);
   });
 
-  socket.on("error" , err =>{
-    console.log('backend error ' , err);
-  })
+  socket.on("error", (err) => {
+    console.log("backend error ", err);
+  });
 
   socket.on("disconnect", (ev) => {
-    console.log("Socket disconnected" , ev);
+    console.log("Socket disconnected", ev);
   });
 });
