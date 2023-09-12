7f9ef389849c4ea8267d9703cd33f7dd09a9a91c
ujual <ujualk2000@gmail.com>
2023-09-12 13:45:48 +0530

    cors error

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "100mb", extended: true }));
 
 
 
-app.use(cors);
+app.use(cors());
 app.use(express.static("public"));
 
 const userRouter = require("./routes/users.js");
