959035da8a495dc835558e6410066336db674622
ujual <ujualk2000@gmail.com>
2023-09-15 20:21:25 +0530

    reviews

--- controller/turfAdminController.js
+++ controller/turfAdminController.js
@@ -27,13 +27,14 @@ const signUp = async (req, res, next) => {
           console.log(error);
         });
       let turfdetail = await turfModel.findOne({ email: turfdetails.email });
-      res.json({ status: true, result: turfdetails });
       usercontroller.sendVerifyMail(
         turfdetails.name,
         turfdetails.email,
         turfdetail._id,
         false
       );
+      res.json({ status: true, result: turfdetails });
+    
     } else {
       return res.json({ error: "User already exists" });
     }
